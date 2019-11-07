396ac2cdaa2dc5384478f5562b1f1c9e0dd07290
TanimaRanjan <tanimaranjan@gmail.com>
2019-11-07 15:26:25 -0500

    App files changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom'
+
+import HomePage from './pages/homepage/homepage'
+import ShopPage from './pages/shop-page/shop-page'
+import Header from './components/header/header'
+import SignInUp from './pages/sign-in-up/sign-in-up'
+import {auth , createUserProfileDocument} from './firebase/firebase.util'
+
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor() {
+    super() 
+
+    this.state={
+      currentUser:null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id:snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({
+          currentUser:userAuth
+        })
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header currentUser={this.state.currentUser}/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signInUp' component={SignInUp} />
+      </Switch>
     </div>
   );
+  }
 }
 
 export default App;
