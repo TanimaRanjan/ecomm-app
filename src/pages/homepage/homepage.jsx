d9d9fbd464eb0d3987a8593f73cf4dce04f2585f
TanimaRanjan <tanimaranjan@gmail.com>
2019-11-07 15:27:25 -0500

    Homepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import MenuDirectory from '../../components/directory/menu-directory'
+
+import './homepage.scss'
+
+const HomePage = () => {
+    return (
+    <div className="homepage">
+        <MenuDirectory />
+    </div>
+    )
+}
+
+export default HomePage
