e08bb92dcbe66323a9d19dfa3db655098023248d
TanimaRanjan <tanimaranjan@gmail.com>
2019-11-09 21:15:32 -0500

    updated cart utils to group item in cart

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+
+import CustomButton from '../custom-button/custom-button'
+import {addItem} from '../../redux/cart/cart.action'
 
 import './collection-item.scss'
 
-const CollectionItem = ({name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item
+    return (
     <div className="collection-item">
         <div className="collection-image" 
         style={{backgroundImage:`url(${imageUrl})`}}>
-            <button className="collection-cart">Add to cart</button>
         </div>
         <div className="collection-footer">
         <span className="collection-name">{name.toUpperCase()}</span>
         <span className="collection-price">{price}</span>
         </div>
+        <CustomButton 
+            onClick={() => addItem(item)}
+            inverted >ADD TO CART</CustomButton>
     </div>
     
-)
+)}
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+    addItem: (item) => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,14 +1,21 @@
 import {CartActionType} from './cart.type'
-
+import {addItemToCart} from './cart.util'
 const INITIAL_STATE = {
-    hidden:true
+    hidden:true,
+    cartItems:[]
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case CartActionType.TOGGLE_CART_HIDDEN:
             return {
+                ...state,
                 hidden:!state.hidden
+            }
+        case CartActionType.ADD_ITEM :
+            return {
+                ...state,
+                cartItems:addItemToCart(state.cartItems, action.payload)
             }
         default :
             return state
