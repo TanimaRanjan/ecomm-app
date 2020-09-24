c8690391ec57d2988b1369011a034c35ce1a6e3f
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-24 13:42:34 -0400

    Home page update

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-
 import MenuDirectory from '../../components/directory/menu-directory'
-
-// import './homepage.scss'
 import {HomePageContainer} from './homepage.styles'
 
 const HomePage = () => {
     return (
-    <HomePageContainer>
-        <MenuDirectory />
-    </HomePageContainer>
+        <HomePageContainer>
+            <MenuDirectory/>
+        </HomePageContainer>
     )
 }
 
